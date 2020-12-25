6d17e66d0948854933a04192fe31f4041f085908
Nicholas D. Horne <55657484+ndhorne@users.noreply.github.com>
2020-12-25 10:48:49 -0800

    Revamped quote object data structure

--- randomquote.js
+++ randomquote.js
@@ -35,48 +35,207 @@ quoteElement.style.backgroundColor = "whitesmoke";
 quoteElement.style.maxWidth =
   containerElement.getBoundingClientRect().width * 0.95 + "px";
 
+quoteElement.title =
+  "Ephemeral Random Quotes Ajax Demonstration " +
+  "(scrollable w/ mouse wheel)";
+
 footerElement.style.display = "inline";
 
-async function setQuote(index) {
+function isCharAtEnd(str, char) {
+  if (typeof(str) != "string" || typeof(char) != "string") {
+    console.error("One or more arguments invalid,",
+      "strings expected at both parameters, returning false.");
+    
+    //no match so return false
+    return false;
+  }
+  
+  if (char.length > 1) {
+    console.error("Single character expected at second parameter,",
+      "only using first character of supplied string argument.");
+  }
+  
+  let regEx = new RegExp(char[0]);
+  
+  return regEx.test(str.slice(-1));
+}
+
+function buildName(nameObj) {
+  let nameStr = "";
+  
+  if (nameObj.prefix) {
+    nameStr += nameObj.prefix;
+  }
+  
+  if (nameObj.first) {
+    if (nameStr) {
+      nameStr += isCharAtEnd(nameStr, " ") ? "" : " ";
+    }
+    
+    nameStr += nameObj.first;
+  }
+  
+  if (nameObj.middle) {
+    if (typeof nameObj.middle == "string") {
+      if (nameStr) {
+        nameStr += isCharAtEnd(nameStr, " ") ? "" : " ";
+      }
+      
+      nameStr +=  nameObj.middle;
+    } else if (typeof nameObj.middle == "object") {
+      for (let middleName of nameObj.middle) {
+        if (nameStr) {
+          nameStr += isCharAtEnd(nameStr, " ") ? "" : " ";
+        }
+        
+        nameStr += middleName;
+      }
+    }
+  }
+  
+  if (nameObj.last) {
+    if (nameStr) {
+      nameStr += isCharAtEnd(nameStr, " ") ? "" : " ";
+    }
+    
+    nameStr += nameObj.last;
+  }
+  
+  nameStr = nameStr.trim();
+  nameStr = nameStr.replace(/\b(\w)\b/g, "$1.");
+  nameStr = nameStr.replace(/ {2,}/g, " ");
+  
+  return nameStr;
+}
+
+function buildDate(dateObj) {
+  let dateStr = "";
+  
+  if (dateObj.day) {
+    dateStr += dateObj.day;
+  }
+  
+  if (dateObj.month) {
+    if (dateStr) {
+      dateStr += isCharAtEnd(dateStr, " ") ? "" : " ";
+    }
+    
+    dateStr += dateObj.month;
+  }
+  
+  if (dateObj.year) {
+    if (dateStr) {
+      dateStr += isCharAtEnd(dateStr, " ") ? "" : " ";
+    }
+    
+    dateStr += dateObj.year;
+  }
+  
+  dateStr = dateStr.trim();
+  dateStr = dateStr.replace(/ {2,}/g, " ");
+  
+  return dateStr;
+}
+
+async function initQuotes() {
   try {
     let response = await fetch("quotes.json");
     let quotes = JSON.parse(await response.text());
+    let index, quoteTimeout, previousIndices = [];
     
-    if (typeof index != "number"
-        || index < 0
-        || index >= quotes.length) {
-      index = Math.floor(Math.random() * quotes.length);
-    }
-    
-    let quote = quotes[index];
-    
-    qElement.innerHTML = quote.quote;
-    footerElement.innerHTML = "— " + quote.author;
-    
-    
-    if (quote.source) {
-      citeElement.innerHTML = quote.source;
-      footerElement.innerHTML += ", ";
-      footerElement.appendChild(citeElement);
-    }
-    
-    if (quote.date) {
-      footerElement.innerHTML += ", " + quote.date;
-    }
-    
-    quoteElement.style.width = "";
-    quoteElement.style.width = Math.max(
-      qElement.getBoundingClientRect().width,
-      footerElement.getBoundingClientRect().width) + "px";
-    quoteElement.style.visibility = "visible";
-    
-    index = ++index % quotes.length;
-    
-    //setTimeout(() => setQuote(index), 15000);
+    return {
+      setQuote: function(indexArg) {
+        if (typeof indexArg != "number"
+            || indexArg < 0
+            || indexArg >= quotes.length) {
+          if (previousIndices.length == quotes.length) {
+            previousIndices = [];
+          }
+          
+          let randomIndex;
+          
+          do {
+            randomIndex = Math.floor(Math.random() * quotes.length);
+          } while (previousIndices.includes(randomIndex));
+          
+          previousIndices.push(randomIndex);
+          index = randomIndex;
+        } else {
+          index = indexArg;
+        }
+        
+        let quote = quotes[index];
+        
+        quoteElement.style.visibility = "hidden";
+        
+        qElement.innerHTML = quote.quote;
+        
+        if (quote.author) {
+          if (quote.author.name) {
+            footerElement.innerHTML = "— " +
+              buildName(quote.author.name);
+          }
+        }
+        
+        if (quote.source) {
+          if (quote.source.title) {
+            citeElement.innerHTML = quote.source.title;
+            footerElement.innerHTML += ", ";
+            footerElement.appendChild(citeElement);
+          }
+          
+          if (quote.source.date) {
+            footerElement.innerHTML += ", " +
+              buildDate(quote.source.date);
+          }
+        }
+        
+        quoteElement.style.width = "";
+        quoteElement.style.width = Math.max(
+          qElement.getBoundingClientRect().width,
+          footerElement.getBoundingClientRect().width) + "px";
+        quoteElement.style.visibility = "visible";
+      },
+      scrollQuote: function(event) {
+        event.preventDefault();
+        clearTimeout(quoteTimeout);
+        
+        if (event.deltaY < 0) {
+          this.setQuote(
+            (--index < 0)
+            ? index += quotes.length
+            : index % quotes.length
+          );
+        } else {
+          this.setQuote(
+            ++index % quotes.length
+          );
+        }
+        
+        this.setQuoteTimeout();
+      },
+      setQuoteTimeout: function() {
+        quoteTimeout = setTimeout(() => {
+          //random quotes
+          this.setQuote();
+          
+          //sequential quotes
+          //this.setQuote(++index % quotes.length);
+          
+          this.setQuoteTimeout();
+        }, 15000);
+      }
+    };
   } catch (e) {
     console.error(e);
     quoteElement.remove();
   }
 }
 
-setQuote();
+initQuotes()
+.then(function(result) {
+  let boundScrollQuote = result.scrollQuote.bind(result);
+  quoteElement.addEventListener("wheel", boundScrollQuote, true);
+  result.setQuote();
+  result.setQuoteTimeout();
+});
